73307b48bf4b323157517f7769191ba6d252e704
Ashwinn11 <ashwinnanbazhagan@gmail.com>
2024-04-05 17:34:01 +0530

    shortlist

--- src/main/java/com/blood/rescue/kafka/consumer/KafkaConsumer.java
+++ src/main/java/com/blood/rescue/kafka/consumer/KafkaConsumer.java
@@ -1,4 +1,13 @@
 package com.blood.rescue.kafka.consumer;
 
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class KafkaConsumer {
+
+    @KafkaListener(topics = "donor-available",groupId = "blood-donor")
+    public void listen(String message){
+        System.out.println("Sent->"+message);
+    }
 }
--- src/main/java/com/blood/rescue/kafka/producer/KafkaProducer.java
+++ src/main/java/com/blood/rescue/kafka/producer/KafkaProducer.java
@@ -1,4 +1,15 @@
 package com.blood.rescue.kafka.producer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class KafkaProducer {
+    @Autowired
+    private KafkaTemplate<String,Object> kafkaTemplate;
+
+    public void sendMessage(String topic,String message){
+        kafkaTemplate.send(topic,message);
+    }
 }
